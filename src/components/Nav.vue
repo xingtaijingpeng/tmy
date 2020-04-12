ae4be4dab530049e6ff5dda2732c2f00be58f7a9
duan <1250101915@qq.com>
2020-04-13 06:37:38 +0800

    a

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,13 +1,22 @@
 <template>
-  <div class="hello">
-    1111111
+  <div>
+    <div class="content logo">
+      <img src="/static/logo.png" alt="">
+      <div class="nav-right">
+        <span class="search"></span>
+        <span class="shoppingCart"></span>
+      </div>
+    </div>
+    <div class="nav">
+        <div class="content"></div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Nav',
-  props: {
+  prop: {
     msg: String
   }
 }
@@ -15,5 +24,37 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
+  .logo{
+    height: 140px;
+    line-height: 140px;
+    border-bottom: 1px solid #ccc;
+  }
+  .logo img{
+      width: 150px;
+  }
+  .nav-right{
+    float: right;
+  }
+  .shoppingCart{
+      display: inline-block;
+      width: 30px;
+      height: 30px;
+      background: url("/static/shoppingCart.png") no-repeat;
+      background-size: 30px;
+  }
+  .search{
+    display: inline-block;
+    width: 30px;
+    height: 30px;
+    background: url("/static/search.png") no-repeat;
+    background-size: 30px;
+  }
+
+  .nav{
+    width: 100%;
+    height:70px;
+    background: #7d1d28;
+  }
+
 
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
-  <div class="home">
+  <div>
     <Nav></Nav>
-    <img alt="Vue logo" src="../assets/logo.png">
   </div>
 </template>
 
